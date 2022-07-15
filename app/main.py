8a14dfbd0e4be1adc3ed5617045da7eda18ba9c9
Stefan Guericke <stefan.guericke@gmail.com>
2022-07-15 10:38:05 +0200

    linter fixes

--- app/main.py
+++ app/main.py
@@ -1,17 +1,24 @@
+"""
+Testimplementation of a Restful API
+"""
 from flask import Flask
 from flask_restful import Api, Resource
-  
-app =   Flask(__name__)
-  
+app = Flask(__name__)
+
 api =   Api(app)
-  
+
 class HelloWorld(Resource):
+    """
+    Testclass
+    """
     def get(self):
+        """
+        Getter for hello world
+        """
         data={"data":"Hello World"}
         return data
-  
+
 api.add_resource(HelloWorld,'/hello')
-  
-  
+
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=8080) #, debug=True)
