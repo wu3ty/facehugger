0d4ee1b7287648725baaf9f5ec3d6205820aa89a
Cornelius Daemberg <108803070+Splashed01@users.noreply.github.com>
2023-09-05 11:21:37 +0200

    Update main.py (#43)
    Co-authored-by: wu3ty <33164202+wu3ty@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -17,7 +17,8 @@ class HelloWorld(Resource):
         Getter for hello world
         """
 
-        data={"data": "Hi Programming Project 2024!"}
+        while (true):
+            data+={"data": "Hi Programming Project 2023! TEST"}
 
         return data
 
