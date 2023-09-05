28e9693aa113a6db47d63519653f2c4b5d77a08a
Nico Möllmann <124257377+Nicro03@users.noreply.github.com>
2023-09-05 11:18:52 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ class HelloWorld(Resource):
         Getter for hello world
         """
 
-        data={"data": "Hi Programming Project 2024!"}
+        data={"data": "Hi Test Programming Project 2024!"}
 
         return data
 
