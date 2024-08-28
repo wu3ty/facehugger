1d6f25db8361ad637e27366bc1c3e9ee9a1224ee
Stefan Guericke <stefan.guericke@gmail.com>
2024-08-28 11:44:39 +0200

    linting fixes

--- app/main.py
+++ app/main.py
@@ -17,36 +17,11 @@ class HelloWorld(Resource):
         Getter for hello world
         """
 
-
         data = {"data": "Hi Programming Project 2024! TEST abc"}
-
 
         return data
 
-class NewEndpoint(Resource):
-    def get(self):
-        # Creating an HTML response using make_response
-        html_content = """
-                <html>
-                    <head>
-                        <title>Felix's Page</title>
-                    </head>
-                    <body>
-                        <h1>Hier ist Felix Endpoint</h1>
-                        <h2>Welcome to Felix's custom endpoint!</h2>
-                    </body>
-                </html>
-                """
-        # Make a response object and set the content type to 'text/html'
-        response = make_response(html_content)
-        response.headers['Content-Type'] = 'text/html'
-        return response
-
-
-
-
 api.add_resource(HelloWorld,'/hello')
-api.add_resource(NewEndpoint,'/felix')
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=CFG_PORT)
