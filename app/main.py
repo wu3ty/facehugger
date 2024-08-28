de5e4ec571ed7fd81e3fa0616bbfb61d668db85e
Marcel Pöppe <mp@MARCEL-LAPTOP>
2024-08-28 11:21:16 +0200

    Die Zunkunft kommt

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ class HelloWorld(Resource):
         Getter for hello world
         """
 
-        data = {"data": "Hi Programming Project 2024! TEST"}
+        data = {"data": "Hi Programming Project 2025! TEST"}
 
         return data
 
