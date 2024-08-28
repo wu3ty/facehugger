1e638b037d515265896de315b295cee84b4305fd
wu3ty <33164202+wu3ty@users.noreply.github.com>
2024-08-28 11:49:53 +0200

    Test 1 (#71)
    * Jahr geändert
    
    * fixed dockerfile
    
    * fixed python
    
    * Added New Route Felix
    
    * specified python3.10
    
    * linting fixes
    
    * allow system install
    
    * removed endpoint
    
    * unused import
    
    ---------
    
    Co-authored-by: Felixfailw <98382575+Felixfailw@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 """
 Test implementation of a Restful API
 """
-from flask import Flask, make_response
+from flask import Flask
 from flask_restful import Api, Resource
 
 CFG_PORT = 5001
@@ -17,36 +17,11 @@ class HelloWorld(Resource):
         Getter for hello world
         """
 
-
         data = {"data": "Hi Programming Project 2024! TEST abc"}
-
 
         return data
 
-class NewEndpoint(Resource):
-    def get(self):
-        # Creating an HTML response using make_response
-        html_content = """
-                <html>
-                    <head>
-                        <title>Felix's Page</title>
-                    </head>
-                    <body>
-                        <h1>Hier ist Felix Endpoint</h1>
-                        <h2>Welcome to Felix's custom endpoint!</h2>
-                    </body>
-                </html>
-                """
-        # Make a response object and set the content type to 'text/html'
-        response = make_response(html_content)
-        response.headers['Content-Type'] = 'text/html'
-        return response
-
-
-
-
 api.add_resource(HelloWorld,'/hello')
-api.add_resource(NewEndpoint,'/felix')
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=CFG_PORT)
